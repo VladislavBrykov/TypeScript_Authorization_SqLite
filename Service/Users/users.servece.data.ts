3298da94902a9233102fd1a8963ab82a873bb8ba
user <user@191-205-88-130.user.vivozap.com.br>
2021-08-13 16:35:44 +0300

    test commit

--- Service/Users/users.servece.data.ts
+++ Service/Users/users.servece.data.ts
@@ -5,76 +5,71 @@ import ResTypeid from './utils/type.id';
 import token from './utils/new.token';
 import functionHelpers from './utils/user.service.helpers';
 
-async function serviceLogin(phoneEmail: string, password: string) {
-    const searchUser = await User.findOne({ where: { phoneEmail: phoneEmail, password: password } });
+export default class UserService {
+    constructor() { }
 
-    if (searchUser) {
-        const newToken = await tokenCreator.newTokenCreater(phoneEmail)
-        const newDevice = {
-            phoneEmail,
-            token: newToken,
+    async serviceLogin(phoneEmail: string, password: string) {
+        const searchUser = User.findOne({ where: { phoneEmail: phoneEmail, password: password } });
+
+        if (searchUser) {
+            const newToken = await tokenCreator.newTokenCreater(phoneEmail);
+            const newDevice = {
+                phoneEmail,
+                token: newToken,
+            }
+            await UserDevice.create(newDevice);
+            return newToken;
         }
-        await UserDevice.create(newDevice);
-        return newToken;
+        return ('login error');
     }
-    return ('login error');
+
+    async serviceRegistration(phoneEmail: string, password: string) {
+        const typeId: string = await ResTypeid(phoneEmail);
+        const newToken: string = await token.newTokenCreater(phoneEmail);
+
+        const registrationUser = {
+            phoneEmail,
+            password,
+            token: newToken,
+            typeId,
+        }
+        const searchUser = await User.findOne({ where: { phoneEmail: phoneEmail } })
+        if (searchUser)
+            return false;
+        else {
+            await User.create(registrationUser);
+            return true;
+        }
+    }
+
+    async serviceLogout(token: string, all) {
+        const searchUser = await UserDevice.findOne({ where: { token: token } });
+
+        if (searchUser) {
+            all === 'true' ?
+                await UserDevice.update({ token: null }, { where: { token: token } }) :
+                await UserDevice.update({ token: null }, { where: { phoneEmail: searchUser.phoneEmail } });
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    async serviceInfouser(token: string) {
+        const searchUserDvice = await functionHelpers.userWithUpdatedToken(token);
+        if (!searchUserDvice) {
+            return { error: 'user not found' };
+        }
+        const searchUserUniquer = await functionHelpers.searchUserTable(searchUserDvice.phoneEmail);
+        if (!searchUserUniquer) {
+            return { error: 'user not found' };
+        }
+        const informationUser = {
+            id: searchUserDvice.phoneEmail,
+            typeId: searchUserUniquer.typeId,
+            token: searchUserDvice.token,
+        }
+        return (informationUser);
+    }
 }
-
-async function serviceRegistration(phoneEmail: string, password: string): Promise<boolean> {
-    const typeId: string = ResTypeid(phoneEmail);
-    const newToken: string = token.newTokenCreater(phoneEmail);
-
-    const registrationUser = {
-        phoneEmail,
-        password,
-        token: newToken,
-        typeId,
-    }
-    const searchUser = await User.findOne({ where: { phoneEmail: phoneEmail } })
-    if (searchUser)
-        return false;
-    else {
-        await User.create(registrationUser);
-        return true;
-    }
-}
-
-async function serviceLogout(token: string, all) {
-    const searchUser = await UserDevice.findOne({ where: { token: token } });
-
-    if (searchUser) {
-        all === 'true' ?
-            UserDevice.update({ token: null }, { where: { token: token } }) :
-            UserDevice.update({ token: null }, { where: { phoneEmail: searchUser.phoneEmail } });
-        return true;
-    }
-    else {
-        return false;
-    }
-}
-
-async function serviceInfouser(token: string) {
-    const searchUserDvice = await functionHelpers.userWithUpdatedToken(token);
-    if (!searchUserDvice) {
-        return { error: 'user not found' };
-    }
-    const searchUserUniquer = await functionHelpers.searchUserTable(searchUserDvice.phoneEmail);
-    if (!searchUserUniquer) {
-        return { error: 'user not found' };
-    }
-    const informationUser = {
-        id: searchUserDvice.phoneEmail,
-        typeId: searchUserUniquer.typeId,
-        token: searchUserDvice.token,
-    }
-    return (informationUser);
-}
-
-const userControllerData = {
-    serviceLogin,
-    serviceRegistration,
-    serviceLogout,
-    serviceInfouser,
-}
-
-export default userControllerData;
